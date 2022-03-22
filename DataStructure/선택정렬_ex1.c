7e2096ba534722df033552bd741c4c64c1a0a306
Defi <defi622@naver.com>
2022-03-23 01:17:58 +0900

    22.03.23

---
+++ DataStructure/선택정렬_ex1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void){
+    int n[10] = {1, 10, 2, 3, 5, 6, 7, 9, 8, 4};
+    int tmp;
+    int count_compare = 0;
+    int count_swap = 0;
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<i; j++){
+            count_compare++;
+            if(n[i]>n[j]){
+                tmp = n[j];
+                n[j] = n[i];
+                n[i] = tmp;
+                count_swap++;
+            }
+        }
+    }
+    for(int i = 0; i<10; i++) printf("%d ",n[i]);
+    printf("\n");
+    printf("compare_count = %d\nswap_count = %d", count_compare, count_swap);
+    return 0;
+}
